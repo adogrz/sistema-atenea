0e9df0818ddc3efb0599b5195492485539b54335
Adonay Gutiérrez <adonaygutierrez50@gmail.com>
2025-06-22 14:56:43 -0600

    Eliminar el logo antiguo y agregar un nuevo logo con mejore resolucion en formato WebP en la página de inicio de sesión.

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -54,9 +54,9 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                 {/* Sección de la imagen */}
                 <div className="relative hidden bg-muted lg:block">
                     <img
-                        src="/logo-pjt-large.avif"
+                        src="/pjt-logo.webp"
                         alt="Image"
-                        className="absolute inset-0 h-full w-full object-cover object-top dark:brightness-[0.8]"
+                        className="absolute inset-0 h-full w-full object-cover object-top"
                         loading="lazy"
                     />
                 </div>
