74d8e7c4f7325e6ef01d26cf6f2bb768dbc6b73a
Adonay Gutiérrez <adonaygutierrez50@gmail.com>
2025-06-21 18:52:32 -0600

    Refactorizar los componentes de diseño y actualizar los estilos del logotipo para lograr una mejor capacidad de respuesta y compatibilidad con temas

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,8 +1,6 @@
 import { Head, useForm } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler } from 'react';
-
-import LogoPjt from '@/components/icons/LogoPjt';
 import InputError from '@/components/input-error';
 import TextLink from '@/components/text-link';
 import { Button } from '@/components/ui/button';
@@ -10,6 +8,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import AppLogoIcon from '@/components/app-logo-icon';
 
 type LoginForm = {
     email: string;
@@ -44,11 +43,13 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                 <div className="w-full max-w-3xl">
                     <div className="flex flex-col gap-0">
                         <Card className="h-[90vh] p-0">
-                            <CardContent className="grid p-0 md:grid-cols-2 h-full">
-                                <form className="flex flex-col justify-center h-full p-6" onSubmit={submit}>
+                            <CardContent className="grid h-full p-0 md:grid-cols-2">
+                                <form className="flex h-full flex-col justify-center p-6" onSubmit={submit}>
                                     <div className="flex flex-col gap-6">
                                         <div className="flex flex-col items-center text-center">
-                                            <LogoPjt className="block aspect-square size-30 object-contain md:hidden" />
+                                            <div className="my-5 flex items-center justify-center rounded-md md:hidden">
+                                                <AppLogoIcon className="h-auto w-32" />
+                                            </div>
                                             <h1 className="text-2xl font-bold text-pretty">Inicia sesión</h1>
                                         </div>
 
@@ -115,14 +116,14 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                                     </div>
                                 </form>
 
-                                  <div className="relative hidden bg-muted md:block h-full">
-                                        <img
-                                            src="/logo-pjt-large.avif"
-                                            alt="Image"
-                                            className="absolute inset-0 h-full w-full object-cover object-top"
-                                            loading="lazy"
-                                        /> 
-                                    </div>             
+                                <div className="relative hidden h-full bg-muted md:block">
+                                    <img
+                                        src="/logo-pjt-large.avif"
+                                        alt="Image"
+                                        className="absolute inset-0 h-full w-full object-cover object-top"
+                                        loading="lazy"
+                                    />
+                                </div>
                             </CardContent>
                         </Card>
 
