70f00340cee74f46359156b71283d163fae788d5
Adonay Gutiérrez <adonaygutierrez50@gmail.com>
2025-06-24 23:36:48 -0600

    Mejora de accesibilidad al actualizar el texto alternativo de la imagen en login.tsx

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -48,7 +48,12 @@ export default function Login({ status, canResetPassword }: LoginProps) {
 
                 {/* Sección de la imagen */}
                 <div className="relative hidden bg-muted lg:block">
-                    <img src="/pjt-logo.webp" alt="Image" className="absolute inset-0 h-full w-full object-cover object-top" loading="lazy" />
+                    <img
+                        src="/pjt-logo.webp"
+                        alt="Logo de El Programa Jóvenes Talentos"
+                        className="absolute inset-0 h-full w-full object-cover object-top"
+                        loading="lazy"
+                    />
                 </div>
             </div>
         </>
